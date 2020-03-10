21268d039b150d959c2cb164be15ac4b12ca7d74
Yehoshua Oliveira <ioxua@newgo.com.br>
2020-03-09 21:49:51 -0300

    feat: removes example entities

--- src/main/java/br/edu/ioxua/rarch/core/position/Position.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ioxua.rarch.core.position;
-
-import br.edu.ioxua.rarch.core.entity.NamedEntityImpl;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Position extends NamedEntityImpl {
-}
--- src/main/java/br/edu/ioxua/rarch/core/user/role/Role.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ioxua.rarch.core.user.role;
-
-import br.edu.ioxua.rarch.core.entity.NamedEntityImpl;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Role extends NamedEntityImpl {
-}
